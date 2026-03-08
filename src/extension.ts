3a9076941676fdd3bdae3ccd34b43dd7c172802d
yabu1121 <n.hayato0122@outlook.jp>
2026-03-08 16:38:16 +0900

    com

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// 引数にvscode.Extention型のcontextを持つ。
+// activate関数はサーバーの起動処理。
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	// 作業中のコンソールで出力する、ヘルスチェック
 	console.log('Congratulations, your extension "time" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	// disposable: 廃棄可能、
+	// vscode.commandはvscodeの機能を呼び出すためのapi registerCommandで自分のものを登録できる。
+	// エンドポイントは'time.helloWorld'でそれが呼ばれたら関数内を実行する。
 	const disposable = vscode.commands.registerCommand('time.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+		// vscode.windowでvscodeのないのwindowのライブラリ、
+		// showInformationMessageは右下のトーストメッセージとして表示される。
 		vscode.window.showInformationMessage('Hello World from time!');
 	});
 
+	// contextは起動したときに渡してくれるストレージやパス、いつ終わるかの管理、
+	// subscriptionはdispose()関数を行う（リソースの解放）
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
